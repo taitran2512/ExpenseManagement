305a1d5c3babb1bd170194dab65ce8e1b5888135
taitran2512 <trantantai25121998@gmail.com>
2021-02-17 17:26:33 +0700

    hash password

--- Server/controller/UserController.js
+++ Server/controller/UserController.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../model/User.model");
 function ResultModel(status, message, data = null) {
 	return {
@@ -21,34 +23,54 @@ router.route("/signup").post((req, res) => {
 	const username = req.body.username;
 	const password = req.body.password;
 	const dob = req.body.dob;
-
-	const newUser = new User({ username, dob, email, phone });
-	User.findOne({ username: username })
-		.then((data) => {
-			if (data) {
-				res.json(ResultModel("error", "Tài khoản đã tồn tại"));
-			} else {
-				newUser
-					.save()
-					.then(() => res.json(ResultModel("success", "Đăng kí thành công", newUser)))
-					.catch((err) => res.status(400).json("Error:", err));
-			}
-		})
-		.catch((err) => res.status(400).json("Error", err));
+	const email = req.body.email;
+	const phone = req.body.phone;
+	//hash password
+	bcrypt.hash(password, 10, (err, hashPassword) => {
+		if (err) {
+			res.json({ error: err });
+		} else {
+			const newUser = new User({ username, password: hashPassword, dob, email, phone });
+			User.findOne({ username: username })
+				.then((data) => {
+					if (data) {
+						res.json(ResultModel("error", "Tài khoản đã tồn tại"));
+					} else {
+						newUser
+							.save()
+							.then(() => {
+								return res.json(ResultModel("success", "Đăng kí thành công", newUser));
+							})
+							.catch((err) => res.status(400).json("Error:", err));
+					}
+				})
+				.catch((err) => res.status(400).json("Error", err));
+		}
+	});
 });
 
 ///////////////đăng nhập////////////////////
 router.route("/login").post((req, res) => {
 	const username = req.body.username;
 	const password = req.body.password;
-	const newUser = new User({ username, dob, email, phone });
-	
-	User.findOne({ username: username, password: password })
-		.then((data) => {
-			if (data) {
-				res.json(ResultModel("success", "Đăng nhập thành công", newUser));
+
+	const newUser = new User({ username });
+
+	User.findOne({ username: username })
+		.then((user) => {
+			if (user) {
+				bcrypt.compare(password, user.password, (err, result) => {
+					if (err) {
+						res.json({ error: err });
+					}
+					if (result) {
+						res.json(ResultModel("success", "Đăng nhập thành công", newUser));
+					} else {
+						res.json(ResultModel("error", "Tài khoản hoặc mật khẩu không chính xác"));
+					}
+				});
 			} else {
-				res.json(ResultModel("error", "Đăng nhập thất bại"));
+				res.json(ResultModel("error", "Tài khoản hoặc mật khẩu không chính xác"));
 			}
 		})
 		.catch((err) => res.status(400).json("Error:", err));
