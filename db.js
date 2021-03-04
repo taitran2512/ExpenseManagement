4b0afb794e99a4b0c399c92ffac139fcfe441d7c
taitran2512 <trantantai25121998@gmail.com>
2021-03-04 15:58:51 +0700

    cm

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-	"mongodb://localhost:27017/expense",
+	"mongodb://uxculcdj4dctyufofzhi:Ig60NJRQ2SSKicfE7GCf@bmbff87enr8qlrq-mongodb.services.clever-cloud.com:27017/bmbff87enr8qlrq",
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const cors = require("cors");
 app.use(cors()); //https://topdev.vn/blog/cors-la-gi/
 app.use(bodyParser.urlencoded({ extended: true }));
