26489a922ed7646a8917f704743d1dfd228f979c
taitran2512 <trantantai25121998@gmail.com>
2021-03-04 15:57:31 +0700

    cm

--- Mobile/react-native.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  project: {
-    ios: {},
-    android: {}, // grouped into "project"
-  },
-  assets: ["./src/res/fonts"], // stays the same
-};
--- Mobile/src/redux/action/history/getHistoryAction.js
+++
@@ -1,9 +0,0 @@
-export const GET_HISTORY = 'GET_HISTORY';
-export const GET_HISTORY_SUCCESS = 'GET_HISTORY_SUCCESS';
-export const GET_HISTORY_ERROR = 'GET_HISTORY_ERROR';
-
-export const getHistoryAction = () => {
-   return {
-      type: GET_HISTORY,
-   };
-};
