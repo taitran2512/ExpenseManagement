6a1e0eb5b4dedc2402c308d12b2b0ccc34b7f714
taitran2512 <trantantai25121998@gmail.com>
2021-03-05 10:06:58 +0700

    add code push ios

--- Mobile/ios/ExpenseManagement/AppDelegate.m
+++ Mobile/ios/ExpenseManagement/AppDelegate.m
@@ -3,6 +3,10 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+//code push
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -27,6 +31,11 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+//code push
+[AppCenterReactNative register];
+[AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+[AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
