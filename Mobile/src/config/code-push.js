9d9ab47a0d807f71927532dc4c0f2aa5724da840
NTA-Pro <ntapro1412@gmail.com>
2021-04-02 13:35:34 +0700

    tắt alert

--- Mobile/src/config/code-push.js
+++ Mobile/src/config/code-push.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Alert } from 'react-native';
 import CodePush from 'react-native-code-push';
 import DownloadView from '../component/custom/DownloadView';
-import LoadingView from '../component/custom/LoadingView';
 
 //auto check when open app
 const CODE_PUSH_OPTIONS = {
@@ -17,20 +16,20 @@ const withCodePush = (WrappedComponent) => {
       componentDidMount() {
          CodePush.sync(
             {
-               updateDialog: {
-                  title: 'Thông báo cập nhật',
-                  optionalIgnoreButtonLabel: 'Để sau',
-                  optionalInstallButtonLabel: 'Cập nhật',
-                  optionalUpdateMessage: 'Đã có bản cập nhật mới, bạn vui lòng cập nhật ứng dụng',
-               },
+               // updateDialog: {
+               //    title: 'Thông báo cập nhật',
+               //    optionalIgnoreButtonLabel: 'Để sau',
+               //    optionalInstallButtonLabel: 'Cập nhật',
+               //    optionalUpdateMessage: 'Đã có bản cập nhật mới, bạn vui lòng cập nhật ứng dụng',
+               // },
                installMode: CodePush.InstallMode.IMMEDIATE,
             },
             (status) => {
                if (status === CodePush.SyncStatus.UPDATE_INSTALLED) {
                   this.setState({ loading: false });
-                  setTimeout(() => {
-                     Alert.alert('Thông báo', 'Cập nhật ứng dụng thành công');
-                  }, 10);
+                  // setTimeout(() => {
+                  //    Alert.alert('Thông báo', 'Cập nhật ứng dụng thành công');
+                  // }, 10);
                }
             },
             (progress) => {
